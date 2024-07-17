c0320b53c05e4ce5697471cd5e01c0e34462c15f
COllins <collinsmuiruri77@gmail.com>
2024-07-17 20:14:54 +0300

    Completes About section

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-teal-800 via-teal to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde iste,
+          minima, obcaecati perspiciatis, veniam itaque velit at autem dolorum
+          illum deserunt possimus corporis odit sunt? Iure quam velit autem
+          dicta consequuntur nam ab dolore libero nihil, culpa explicabo,
+          labore, enim possimus provident. Obcaecati odio quos excepturi
+          exercitationem sint, quidem sunt.
+        </p>
+
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
+          perferendis necessitatibus architecto, debitis eligendi unde sunt vel
+          asperiores ratione veritatis aliquid magnam aut impedit optio. Enim
+          necessitatibus accusantium sint autem architecto ducimus hic harum
+          sequi eum! Pariatur molestias neque voluptas veritatis iusto est.
+          Sequi iure voluptatem laudantium libero dolore! Exercitationem!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
